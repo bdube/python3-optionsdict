0579c19f72ac416761915704f138b627d7621776
Brian Dube <brian.dube@omax.com>
2018-09-27 01:53:27 +0000

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python3-optionsdict
 Simple key=value parser
+
+Testing GitHub integration with Eclipse Che
---
+++ optionsdict/__init__.py
@@ -0,0 +1,48 @@
+
+# optionsdict: The tiny parser
+
+def string_to_python(sval):
+    if sval is None: return None
+    if sval == 'true': return True
+    if sval == 'false': return False
+
+def parse(args, defaults={}):
+    """Converts a list of 'key=value' command line arguments to a
+       dictionary.
+       
+       The Python values True, False, and None may be set
+       case-insensitively. To delete a key foo, use the syntax
+       'foo=' with no value. One standalone argument may be given
+       without the '=' separator, and this argument will be
+       mapped to the key named _arg. If more than one standalone
+       argument is given, the last one wins.
+       
+       Values that convert to integer without error are stored
+       as integers.
+       
+       Returns the result of applying supplied arguments to a
+       dictionary of default values.
+    """
+    parsed = defaults.copy()
+    if args is None:
+        return parsed
+    for i in range(len(args)):
+        head, sep, tail = args[i].partition('=')
+        if not sep:
+            key = '_arg'
+            parsed[key] = head
+            continue
+        if not tail:
+            if head in parsed:
+                del parsed[head]
+            continue
+        ltail = tail.lower()
+        if ltail in ['true', 'false', 'none']:
+            parsed[head] = string_to_python(ltail)
+            continue
+        try:
+            tail = int(tail)
+        except ValueError:
+            pass
+        parsed[head] = tail
+    return parsed
